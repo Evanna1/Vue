a95bf22e47464a680207cee8545f6ffe664c075d
evanna <1303370223@qq.com>
2025-05-18 19:06:31 +0800

    删除了不必要信息

--- tokenblock.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from __init__ import db
-
-class TokenBlocklist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    jti = db.Column(db.String(40), nullable=False, index=True)
-    created_at = db.Column(db.DateTime, nullable=False)
