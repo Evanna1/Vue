fc5bc81250c812ce091ac7df35ccc39050fe92ba
qadty12138 <2328375531@qq.com>
2025-05-18 12:18:50 +0800

    Update PostDetail.vue

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -29,7 +29,7 @@ onMounted(async () => {
   try {
     const response = await axios.get(`http://127.0.0.1:5000/user/article_content/${postId}`, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('jwt_token')}`,
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
     })
     post.value = response.data.article
